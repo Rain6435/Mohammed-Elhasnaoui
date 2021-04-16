e3a2ce2dbd4eae2a6020b0d31387b49c0e9297f4
Mohammed Elhasnaoui <77762120+Rain6435@users.noreply.github.com>
2021-04-16 13:55:08 -0400

    Update Notes.h

--- Notes.h
+++ Notes.h
@@ -8,16 +8,20 @@
 #include "Cours.h"
 using namespace std;
 
-class Notes : public Cours, public Student
-{
+
+//Création de la classe Notes avec comme héritage la classe Cours et la classe Student
+class Notes : public Cours, public Student{
+    // Ajout des opérateurs d'entrée et de sortie
     friend istream& operator>>(istream&, Notes&);				
     friend ostream& operator<<(ostream&, const Notes&);			
 public:
+    //Définition des objets de la classe 
     string Identifier;
     float Note;
     Cours::CourseID;
     Student::StudentID;
 
+ //Définition du constructeur paramètré
 public:Notes(string Ident, float StNotes, int StID, int CrID) {
     Identifier = Ident;
     Note = StNotes;
