34240ddc65dec4b6d5834961a2a6614e7e264572
Mohammed Elhasnaoui <77762120+Rain6435@users.noreply.github.com>
2021-04-16 13:51:30 -0400

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,72 +1,30 @@
-#include "Student.h"
-#include "Cours.h"
-#include "Notes.h"
+#pragma once
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
 
 
-istream& operator>>(istream& istr, Student& r) {
-	istr >> r.StudentFName;
-	std::cout << endl;
-	istr >> r.StudentName;
-	std::cout << endl;
-	istr >> r.StudentID;
-	std::cout << endl;
-	return istr;
+//Création de la classe Student
+class Student {
+    // Ajout des opérateurs d'entrée et de sortie
+    friend istream& operator>>(istream&, Student&);				
+    friend ostream& operator<<(ostream&, const Student&);		
+public:
+    //Définition des objets de la classe 
+    int StudentID;
+    string StudentName;
+    string StudentFName;
+//Définition du construteur de défaut
+public:Student();
+//Définition du constructeur paramètré
+public:Student(int ID, string Name, string FName) {
+    StudentID = ID;
+    StudentName = Name;
+    StudentFName = FName;
 }
-
-ostream& operator<<(ostream& ostr, const Student& r) {
-	ostr << r.StudentFName;
-	std::cout << endl;
-	ostr << r.StudentName;
-	std::cout << endl;
-	ostr << r.StudentID;
-	std::cout << endl;
-	
-	return ostr;
-}
-
-istream& operator>>(istream& istr, Cours& r) {
-	istr >> r.CourseID;
-	std::cout << endl;
-	istr >> r.CourseCode;
-	std::cout << endl;
-	istr >> r.CourseTitle;
-	std::cout << endl;
-	return istr;
-}
-
-ostream& operator<<(ostream& ostr, const Cours& r) {
-	ostr << r.CourseID;
-	std::cout << endl;
-	ostr << r.CourseCode;
-	std::cout << endl;
-	ostr << r.CourseTitle;
-	std::cout << endl;
-
-	return ostr;
-}
-
-
-istream& operator>>(istream& istr, Notes& r) {
-	istr >> r.Identifier;
-	std::cout << endl;
-	istr >> r.CourseCode;
-	std::cout << endl;
-	istr >> r.Note;
-	std::cout << endl;
-	istr >> r.StudentID;
-	std::cout << endl;
-	return istr;
-}
-
-ostream& operator<<(ostream& ostr, const Notes& r) {
-	ostr << r.Identifier;
-	std::cout << endl;
-	ostr << r.CourseCode;
-	std::cout << endl;
-	ostr << r.Note;
-	std::cout << endl;
-	ostr << r.StudentID;
-	std::cout << endl;
-	return ostr;
-}
-
+};
+#endif
